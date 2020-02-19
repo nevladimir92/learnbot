18138108085e2ef8691d68e1ff88cdf1aea45e46
Vladimir Nedelkin <nevladimir@ld.yandex.ru>
2020-02-19 20:17:47 +0300

    Первое задание

--- bot.py
+++ bot.py
@@ -1,31 +1,36 @@
-from telegram.ext import Updater, CommandHandler,MessageHandler,Filters
 import logging
+
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
 import settings
 
 PROXY = {'proxy_url': 'socks5h://t1.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-logging.basicConfig(format='%(asсtime)s - %(levelname)s - %(message)s',
-                    level = logging.INFO,
-                    filename = 'bot.log'
-                    )
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO,
+    filename = 'bot.log',
+)
+
+logger = logging.getLogger()
 
 def greet_user(bot, update):
      text ='Вызван/start'
-     logging.info(text)
+     logger.info(text)
      update.message.reply_text(text)
 
 def talk_to_me(bot, update):
-    user_text = "Привет {}, ты написал  {}".format(update.message.chat.first_name, update.message.text)
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
-                                update.message.chat.chat_id,  update.message.chat.text, 
+    user_text = "Привет! {}: Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+    logger.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
+                 update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs = settings.PROXY)
     
-    logging.info("Бот запускается")
+    logger.info("Бот запускается")
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start',greet_user))
     dp.add_handler(MessageHandler(Filters.text,talk_to_me))
